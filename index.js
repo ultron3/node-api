95ed41aeb381f089b8fe97bce4f4ad4387f217c3
alex gnavi <alex.gnavi@gmail.com>
2023-02-21 09:37:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+//creo un collegamento con mongodb
+var MongoClient = require('mongodb').MongoClient; 
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("dbmpalestra");
+  var query = { nome: "alex" };
+  dbo.collection("progressi").find(query).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
---
+++ page.js
@@ -0,0 +1,11 @@
+console.log("hello wolrd")
+
+let docTitle = document.title;
+window.addEventListener("blur",()=>{
+    document.title = "torna qui;(";
+});
+
+window.addEventListener("focus",()=>{
+
+});
+
