99acff89e09b6cadb19033943476c1cde06933aa
alex gnavi <alex.gnavi@gmail.com>
2023-02-21 11:10:33 +0100

    commenti

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("dbmpalestra");
-  var query = { nome: "alex" };
-  dbo.collection("progressi").find(query).toArray(function(err, result) {
+  var dbo = db.db("dbmpalestra");//nome db
+  var query = { nome: "alex" };  //query
+  dbo.collection("progressi").find(query).toArray(function(err, result) {  //nome collection
     if (err) throw err;
     console.log(result);
     db.close();
